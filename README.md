a19355850c744e186135125fa46dce1567d03f8b
Noel Tock <hello@noeltock.com>
2023-09-01 17:19:56 +0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# vpdfo
+# VPDFO
+
+- One-page website built using Tailwind
+- Automatic deploys on push, to vpdfo.org
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'hat': "#ff6b33"
+      },
+      fontFamily: {
+        'staat': ['"Staatliches"', 'sans-serif'],
+        'inter': ['"Inter"', 'sans-serif']
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
